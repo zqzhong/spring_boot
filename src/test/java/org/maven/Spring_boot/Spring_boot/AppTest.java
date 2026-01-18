aee6453dc0e899cbcd65c8c21f9332420c8a4e5a
zqzhong <51041360+zqzhong@users.noreply.github.com>
2026-01-18 11:23:13 +0800

    UT

--- src/test/java/org/maven/Spring_boot/Spring_boot/AppTest.java
+++ src/test/java/org/maven/Spring_boot/Spring_boot/AppTest.java
@@ -2,6 +2,7 @@ package org.maven.Spring_boot.Spring_boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.maven.Spring.console.App;
 import org.maven.Spring.console.entity.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,14 +12,11 @@ import org.springframework.test.context.junit4.SpringRunner;
  * Unit test for simple App.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = App.class)
 public class AppTest {
-	
-	@Autowired
-	Person person;
-	
+
 	@Test
     public void testOne(){
-        System.out.println(person);
+        System.out.println("test");
     }
 }
